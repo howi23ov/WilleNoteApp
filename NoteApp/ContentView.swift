647ee935c3f16189f448f57529e6599e26eb6aa2
wille <howi23ov@student.ju.se>
2025-01-15 14:13:42 +0100

    testing to adding to git

--- NoteApp/ContentView.swift
+++ NoteApp/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("testar ändra text och lägga in i git och se om det funkar!")
+        Text(" testar att se att det funkar att ladda upp ")
             .padding()
     }
 }
