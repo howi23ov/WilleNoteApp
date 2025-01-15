f4a67a599239c154bf2324452b5f8c4d58639a19
wille <howi23ov@student.ju.se>
2025-01-15 13:54:02 +0100

    Initial Commit

---
+++ NoteApp/NoteAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteAppApp.swift
+//  NoteApp
+//
+//  Created by Wille Holgersson on 2025-01-15.
+//
+
+import SwiftUI
+
+@main
+struct NoteAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
